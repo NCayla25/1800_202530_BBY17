b9ef228bcd9d6e8a18131768f3d1217dd11ba51d
Nicholas Cayla <nicholas.cayla@gmail.com>
2025-10-29 12:11:29 -0700

    Made skeleton.html with an interactive footer. Couldn't test because I couldn't get past main.html but we'll see when the authentication is finished.

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+
+
+const navLinks = document.querySelectorAll('footer nav a');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.forEach(l => l.classList.remove('active'));
+        link.classList.add('active');
+    });
+});
